3c73c87efe99a6c0722909a39906c9fef75d44a1
Enes Türk <partuminfernum@gmail.com>
2024-10-27 19:40:48 +0300

    nextjs15: Initial commit

---
+++ nextjs15/app/components/hello.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+function Hello() {
+  console.log("I'm a client-side component");
+
+  return (
+    <div>
+      <h1>Hello</h1>
+      <h1>World</h1>
+    </div>
+  );
+}
+
+export default Hello;
