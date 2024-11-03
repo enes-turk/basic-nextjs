5a056dd4e06638f554d82fffedbf230690300613
Enes Türk <partuminfernum@gmail.com>
2024-11-03 19:00:50 +0300

    seeinfy: page list utils

--- seenify/app/(root)/page.tsx
+++ seenify/app/(root)/page.tsx
@@ -1,6 +1,26 @@
-import SearchForm from "@/app/components/searchForm";
+import SearchForm from "@/components/searchForm";
+import StartupCard from "@/components/startupCard";
 
-export default function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "Enes" },
+      _id: 1,
+      description: "This is a description",
+      img: "https://images.unsplash.com/photo-1670282840604-7789834af47c?q=80&w=2500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      category: "Robots",
+      title: "We Robots",
+    },
+  ];
+
   return (
     <>
       <section className="pink_container">
@@ -12,7 +32,23 @@ export default function Home() {
           Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
           Competitions.
         </p>
-        <SearchForm />
+        <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType, index: number) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No Startups Found</p>
+          )}
+        </ul>
       </section>
     </>
   );
